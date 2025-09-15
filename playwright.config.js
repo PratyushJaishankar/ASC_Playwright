2efdc1efe44db51c18ab1b87986a01d9a4b6a6d6
Pratyush Jaishankar <pratjai97@gmail.com>
2025-09-15 11:27:47 +0530

    Changed Timeout

--- playwright.config.js
+++ playwright.config.js
@@ -15,6 +15,7 @@ import { defineConfig, devices } from '@playwright/test';
 //  checking github
 export default defineConfig({
   testDir: './tests',
+  timeout: 300000,
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
